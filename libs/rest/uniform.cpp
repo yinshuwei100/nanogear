6420cae6d111c4bf960e0722df01f2cb1a6ec618
Lorenzo Villani <lvillani@binaryhelix.net>
2008-08-15 20:35:45 +0000

    * imported header image * uniform class skeleton
    git-svn-id: http://nanogear.googlecode.com/svn/trunk@3 85b8e83e-8254-0410-8bd2-8d79ff186df0
    
    committer: Lorenzo Villani <lvillani@binaryhelix.net>

---
+++ libs/rest/uniform.cpp
@@ -0,0 +1,23 @@
+/*
+ * This file is part of the $PROJECT$ project
+ *
+ * (C) Copyright $YEAR by Lorenzo Villani <lvillani@binaryhelix.net>
+ *
+ */
+#include "uniform.hpp"
+
+namespace nanogear {
+
+namespace rest {
+
+uniform::uniform() {
+}
+
+
+uniform::~uniform() {
+}
+
+
+}
+
+}
---
+++ libs/rest/uniform.hpp
@@ -0,0 +1,23 @@
+#ifndef NANOGEAR_RESTUNIFORM_HPP
+#define NANOGEAR_RESTUNIFORM_HPP
+
+namespace nanogear {
+
+namespace rest {
+
+/**
+ @author Lorenzo Villani <lvillani@binaryhelix.net>
+*/
+class uniform {
+public:
+    uniform();
+
+    ~uniform();
+
+};
+
+}
+
+}
+
+#endif /* NANOGEAR_RESTUNIFORM_HPP */
