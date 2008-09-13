fc79adeee4d8988351d96466ed80a3200a09e99f
Lorenzo Villani <lvillani@binaryhelix.net>
2008-09-13 00:31:08 +0000

    adjust license headers: first run
    git-svn-id: http://nanogear.googlecode.com/svn/trunk@27 85b8e83e-8254-0410-8bd2-8d79ff186df0
    
    committer: Lorenzo Villani <lvillani@binaryhelix.net>

--- libs/rest/uniform.hpp
+++ libs/rest/uniform.hpp
@@ -1,23 +1,26 @@
 /*
- * This file is part of the nanogear project
+ * Nanogear - C++ web development framework
  *
- * (C) Copyright 2008 by Lorenzo Villani <lvillani@binaryhelix.net>
+ * This library is based on Restlet (TM) <http://www.restlet.org> by Noelios Technologies
+ * Copyright (C) 2005-2008 by Noelios Technologies <http://www.noelios.com>
+ * Restlet is a registered trademark of Noelios Technologies. All other marks and
+ * trademarks are property of their respective owners.
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License as published by the Free Software Foundation;
- * version 3 of the License.
+ * Copyright (C) 2008 Lorenzo Villani.
  *
- * This library is distributed in the hope that it will be useful,
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Library General Public License
- * along with this library; see the file COPYING.LIB.  If not, write to
- * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA 02110-1301, USA.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 
 #ifndef NANOGEAR_RESTUNIFORM_HPP
 #define NANOGEAR_RESTUNIFORM_HPP
