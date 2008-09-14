7a31a2ff02f19673c86f1e4fc9016a02bff3c42c
Lorenzo Villani <lvillani@binaryhelix.net>
2008-09-14 17:12:36 +0000

    add challenge_request class
    git-svn-id: http://nanogear.googlecode.com/svn/trunk@46 85b8e83e-8254-0410-8bd2-8d79ff186df0
    
    committer: Lorenzo Villani <lvillani@binaryhelix.net>

---
+++ libs/rest/data/challenge_request.cpp
@@ -0,0 +1,87 @@
+/*
+ * Nanogear - C++ web development framework
+ *
+ * This library is based on Restlet (R) <http://www.restlet.org> by Noelios Technologies
+ * Copyright (C) 2005-2008 by Noelios Technologies <http://www.noelios.com>
+ * Restlet is a registered trademark of Noelios Technologies. All other marks and
+ * trademarks are property of their respective owners.
+ *
+ * Copyright (C) 2008 Lorenzo Villani.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#include <list>
+#include <string>
+
+#include "parameter.hpp"
+
+#include "challenge_request.hpp"
+
+namespace nanogear {
+namespace rest {
+namespace data {
+
+challenge_request::challenge_request(const challenge_scheme& scheme,
+                                     std::string& realm)
+{
+    m_scheme = scheme;
+    m_realm = realm;
+}
+
+bool challenge_request::operator==(const challenge_request& other)
+{
+    bool ret = false;
+    ret = (parameters() == other.parameters());
+    ret = (realm() == other.realm());
+    ret = (scheme() == other.scheme());
+    return ret;
+}
+
+bool challenge_request::operator!=(const challenge_request& other)
+{
+    return !(*this == other)
+}
+
+std::list<parameter>& challenge_request::parameters()
+{
+    return m_parameters;
+}
+void challenge_request::set_parameters(const std::list<parameter>& parameters)
+{
+    m_parameters = parameters;
+}
+
+std::string& challenge_request::realm()
+{
+    return m_realm;
+}
+void challenge_request::set_realm(const std::string& realm)
+{
+    m_realm = realm;
+}
+
+challenge_scheme& challenge_request::scheme()
+{
+    return m_scheme;
+}
+void challenge_request::set_scheme(const challenge_scheme& scheme)
+{
+    m_scheme = scheme;
+}
+
+
+}
+}
+}
---
+++ libs/rest/data/challenge_request.hpp
@@ -0,0 +1,68 @@
+/*
+ * Nanogear - C++ web development framework
+ *
+ * This library is based on Restlet (R) <http://www.restlet.org> by Noelios Technologies
+ * Copyright (C) 2005-2008 by Noelios Technologies <http://www.noelios.com>
+ * Restlet is a registered trademark of Noelios Technologies. All other marks and
+ * trademarks are property of their respective owners.
+ *
+ * Copyright (C) 2008 Lorenzo Villani.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef NANOGEAR_REST_DATA_CHALLENGE_REQUEST_HPP
+#define NANOGEAR_REST_DATA_CHALLENGE_REQUEST_HPP
+
+namespace std {
+class string;
+template <class T> class list;
+}
+
+namespace nanogear {
+namespace rest {
+namespace data {
+
+// forward-declare challenge_scheme
+class challenge_scheme;
+
+class challenge_request {
+public:
+    challenge_request(const challenge_scheme&, const std::string&);
+    virtual ~challenge_request() {};
+
+    bool operator==(const challenge_request&) const;
+    bool operator!=(const challenge_request&) const;
+                        
+    std::list<parameter>& parameters() const;
+    void set_parameters(const std::list<parameter>&);
+    
+    const std::string& realm() const;
+    void set_realm(const std::string&);
+    
+    const challenge_scheme scheme() const;
+    void set_scheme(const challenge_scheme&);
+    
+protected:
+private:
+    challenge_scheme m_scheme;
+    std::string m_realm;
+    std::list<parameter> m_parameters;
+};
+
+
+}
+}
+}
+
+#endif /* NANOGEAR_REST_DATA_CHALLENGE_REQUEST_HPP */
