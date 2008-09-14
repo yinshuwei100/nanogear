0d363cb480dd04ffbe0c980adc32a0e69f9b876a
Lorenzo Villani <lvillani@binaryhelix.net>
2008-09-14 11:54:40 +0000

    updated license headers (second run)
    git-svn-id: http://nanogear.googlecode.com/svn/trunk@42 85b8e83e-8254-0410-8bd2-8d79ff186df0
    
    committer: Lorenzo Villani <lvillani@binaryhelix.net>

--- libs/rest/context.hpp
+++ libs/rest/context.hpp
@@ -1,7 +1,7 @@
 /*
  * Nanogear - C++ web development framework
  *
- * This library is based on Restlet (TM) <http://www.restlet.org> by Noelios Technologies
+ * This library is based on Restlet (R) <http://www.restlet.org> by Noelios Technologies
  * Copyright (C) 2005-2008 by Noelios Technologies <http://www.noelios.com>
  * Restlet is a registered trademark of Noelios Technologies. All other marks and
  * trademarks are property of their respective owners.
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 
 
 
