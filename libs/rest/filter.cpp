98fa490310b2e9df7bbfa562f940d13b0a2de89c
Michael Howell <mhowell123@gmail.com>
2008-09-18 02:09:41 +0000

    Filter abstract class.
    git-svn-id: http://nanogear.googlecode.com/svn/trunk@60 85b8e83e-8254-0410-8bd2-8d79ff186df0
    
    committer: Michael Howell <mhowell123@gmail.com>

---
+++ libs/rest/filter.cpp
@@ -0,0 +1,86 @@
+/*
+ * Nanogear - C++ web development framework
+ *
+ * This library is based on Restlet (R) <http://www.restlet.org> by Noelios Technologies
+ * Copyright (C) 2005-2008 by Noelios Technologies <http://www.noelios.com>
+ * Restlet is a registered trademark of Noelios Technologies. All other marks and
+ * trademarks are property of their respective owners.
+ *
+ * Copyright (C) 2008 Lorenzo Villani.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+
+#include "filter.hpp"
+#include <stdexcept>
+
+namespace nanogear {
+namespace rest {
+
+filter::filter()
+{
+    this(context(), 0);
+}
+
+filter::filter(context& c)
+{
+    this(c, 0);
+}
+
+filter::filter(context& c, controller* n)
+{
+    next = n;
+}
+
+void filter::do_handle(request& req, response& res)
+{
+    if (has_next()) {
+        get_next()->handle(req, res);
+    } else {
+        throw std::runtime_error("do_handle() without a next controller.");
+    }
+}
+
+controller* filter::get_next()
+{
+    return next;
+}
+
+bool filter::has_next()
+{
+    return next != 0;
+}
+
+void filter::set_next(controller* c)
+{
+    next = c;
+}
+
+void filter::handle(request& req, response& res)
+{
+    init(req, res);
+    before_handle(req, res);
+    do_handle(req, res);
+    after_handle(req, res);
+}
+
+}
+}
+
+
+
+
+
+
