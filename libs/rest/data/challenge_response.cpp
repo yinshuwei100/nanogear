c4a8483b31cf34a55cbb7cace4ec8bac139bda65
Lorenzo Villani <lvillani@binaryhelix.net>
2008-09-19 17:10:52 +0000

    challenge_response
    git-svn-id: http://nanogear.googlecode.com/svn/trunk@62 85b8e83e-8254-0410-8bd2-8d79ff186df0
    
    committer: Lorenzo Villani <lvillani@binaryhelix.net>

---
+++ libs/rest/data/challenge_response.cpp
@@ -0,0 +1,101 @@
+/*
+ * Nanogear - C++ web development framework
+ *
+ * This library is based on Restlet (R) <http://www.restlet.org> by Noelios Technologies
+ * Copyright (C) 2005-2008 by Noelios Technologies <http://www.noelios.com>
+ * Restlet is a registered trademark of Noelios Technologies. All other marks and
+ * trademarks are property of their respective owners.
+ *
+ * Copyright (C) 2008 Lorenzo Villani.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "challenge_response.hpp"
+
+namespace nanogear {
+namespace rest {
+namespace data {
+
+challenge_response::challenge_response(const challenge_scheme& scheme,
+                                       const std::string& credentials)
+{
+    m_scheme = scheme;
+    m_credentials = credentials;
+}
+
+challenge_response::challenge_response(const challenge_scheme& scheme,
+                                       const std::string& identifier,
+                                       const std::string& secret)
+{
+    m_scheme = scheme;
+    m_identifier = identifier;
+    m_secret = secret;
+}
+
+bool operator==(const challenge_response& other)
+{
+    bool result = false;
+    result = (scheme() == other.scheme());
+    result = (identifier() == other.identifier());
+    result = (secret() == other.secret());
+    result = (credentials() == other.credentials());
+}
+
+bool operator!=(const challenge_response& other)
+{
+    return !(*this == other);
+}
+
+
+
+
+const challenge_scheme& challenge_response::scheme()
+{
+    return m_scheme;
+}
+void challenge_response::set_scheme(const const challenge_scheme& scheme)
+{
+    m_scheme = scheme;
+}
+
+const std::string& challenge_response::identifier()
+{
+    return m_identifier;
+}
+void challenge_response::set_identifier(const std::string& identifier)
+{
+    m_identifier = identifier;
+}
+
+const std::string& challenge_response::secret()
+{
+    return m_secret;
+}
+void challenge_response::set_secret(const std::string& secret)
+{
+    m_secret = secret;
+}
+
+const std::string& challenge_response::credentials()
+{
+    return m_credentials;
+}
+void challenge_response::set_credentials(const std::string& credentials)
+{
+    m_credentials = credentials;
+}
+
+}
+}
+}
