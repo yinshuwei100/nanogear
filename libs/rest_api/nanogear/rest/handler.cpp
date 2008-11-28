0b0dbe0cf7320d9acfa0d104c40a7830b542e656
Michael Howell <mhowell123@gmail.com>
2008-11-28 04:14:24 +0000

    Beginnings of hander class.
    git-svn-id: http://nanogear.googlecode.com/svn/trunk@197 85b8e83e-8254-0410-8bd2-8d79ff186df0
    
    committer: Michael Howell <mhowell123@gmail.com>

---
+++ libs/rest_api/nanogear/rest/handler.cpp
@@ -0,0 +1,69 @@
+/*
+ * Nanogear - C++ web development framework                                  
+ *
+ * This library is based on Restlet (R) <http://www.restlet.org> by Noelios Technologies
+ * Copyright (C) 2005-2008 by Noelios Technologies <http://www.noelios.com>             
+ * Restlet is a registered trademark of Noelios Technologies. All other marks and
+ * trademarks are property of their respective owners.
+ *
+ * Copyright (C) 2008 Lorenzo Villani.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "handler.hpp"
+
+namespace nanogear {
+namespace rest {
+
+handler::handler(const context& c, const data::request& req, const data::response& res)
+    : m_context(c), m_request(req), m_response(res) {}
+
+bool handler::allow_delete() const
+{
+    return false;
+}
+bool handler::allow_get() const
+{
+    return false;
+}
+bool handler::allow_head() const
+{
+    return allow_get();
+}
+bool handler::allow_options() const
+{
+    return true;
+}
+bool handler::allow_post() const
+{
+    return false;
+}
+bool handler::allow_put() const
+{
+    return false;
+}
+const context& handler::get_context() const
+{
+    return m_context;
+}
+const data::request& handler::get_request() const
+{
+    return m_request;
+}
+const data::response& hander::get_response() const
+{
+    return m_response;
+}
+}
+}
