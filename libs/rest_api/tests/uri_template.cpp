e23f1f21f0dec059d8f5d9c7609ffc85e6d6d9ec
Lorenzo Villani <lvillani@binaryhelix.net>
2008-12-04 19:11:28 +0000

    updated test suite
    git-svn-id: http://nanogear.googlecode.com/svn/trunk@214 85b8e83e-8254-0410-8bd2-8d79ff186df0
    
    committer: Lorenzo Villani <lvillani@binaryhelix.net>

--- libs/rest_api/tests/uri_template.cpp
+++ libs/rest_api/tests/uri_template.cpp
@@ -1,13 +1,29 @@
-
-#include <boost/test/unit_test.hpp>
-#include <nanogear/rest/util/uri_template.hpp>
+#include <string>
+#include <iostream>
 
 #define BOOST_TEST_MODULE uri_template_test
+#include <boost/test/included/unit_test.hpp>
+
+#include <nanogear/rest/util/uri_template.hpp>
 
 using namespace nanogear::rest::util;
 
-BOOST_AUTO_TEST_CASE(simple) {
-    uri_template t("http://www.example.com/users/{userid}");
-    t.set("userid", "fred");
-    
+// Test for URI normalization (i.e: make it all lowercase)
+// BOOST_AUTO_TEST_CASE(normalization) {
+//     uri_template t("hTtp://wWw.EXAMPLE.com/usErs/{uSErID}");
+//     std::string s("http://www.example.com/users/{userid}");
+//     BOOST_CHECK(t.expanded() ==  s);
+// }
+
+BOOST_AUTO_TEST_CASE(variables) {
+    // result with simple assignment (no default value)
+    const std::string result1("http://example.org/?q=fred");
+    uri_template t1("http://example.org/?q={bar}");
+    t1.var()["bar"] = "fred";
+    BOOST_CHECK(t1.expanded() == result1);
+
+    // result with no defined variable
+    const std::string result2("/");
+    uri_template t2("/{xyxyxy}");
+    BOOST_CHECK(t2.expanded() == result2);
 }
