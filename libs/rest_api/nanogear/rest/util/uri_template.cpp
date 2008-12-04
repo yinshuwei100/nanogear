809bd725d4273b64b86339977d83720886bcc020
Lorenzo Villani <lvillani@binaryhelix.net>
2008-12-04 15:03:10 +0000

    -add (incomplete) uri_template class
    git-svn-id: http://nanogear.googlecode.com/svn/trunk@204 85b8e83e-8254-0410-8bd2-8d79ff186df0
    
    committer: Lorenzo Villani <lvillani@binaryhelix.net>

---
+++ libs/rest_api/nanogear/rest/util/uri_template.cpp
@@ -0,0 +1,58 @@
+/*
+ * Nanogear - C++ web development framework
+ * Class to parse URI templates as specified at
+ *   http://bitworking.org/projects/URI-Templates/
+ *
+ * Copyright (C) 2008 Lorenzo Villani.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <map>
+#include <string>
+
+
+
+#include "uri_template.hpp"
+
+namespace nanogear {
+namespace rest {
+namespace util {
+
+std::map<std::string, std::string> uri_template::var()
+{
+    return m_vars;
+}
+
+void uri_template::set_template_string(const std::string& template_string)
+{
+    m_template_string = template_string;
+}
+const std::string& uri_template::template_string() const
+{
+    return m_template_string;
+}
+
+std::string uri_template::expanded()
+{
+    // Step 1: Normalization
+    // TODO: Normalize the URI and %-encode all the character outside the
+    //       reserved space
+    // Parsing and substitutions (done via regexp parser)
+    // Step 2: variable substitutions
+    
+}
+
+}
+}
+}
