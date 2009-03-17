b55f2850fec10e747fd52b8c639ccd26c544f2a6
Michael Howell <mhowell123@gmail.com>
2009-03-16 18:07:34 -0700

    Remove placeholder Request typedef.
    committer: Michael Howell <mhowell123@gmail.com>

--- lib/genheaders.sh
+++ lib/genheaders.sh
@@ -5,5 +5,5 @@ find . -name '*.h' | while read header; do
     HEADER_NAME=`echo $header | sed 's|\.h$||'`
     HEADER_BASENAME=`basename $header`
     echo "Generating $HEADER_NAME..."
-    echo "#include \"./$HEADER_BASENAME\"" >> $HEADER_NAME
+    echo "#include \"./$HEADER_BASENAME\"" > $HEADER_NAME
 done
