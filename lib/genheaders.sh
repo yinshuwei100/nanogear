3ee06d6adb165f628ccf82e9a1cc4dd084ab5091
Lorenzo Villani <lvillani@binaryhelix.net>
2009-04-18 17:17:13 +0200

    It would be a perfect world if only qt-creator's git plugins worked properly :)
    committer: Lorenzo Villani <lvillani@binaryhelix.net>

--- lib/genheaders.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# A simple script to generate Qt-style header names
-
-find . -name '*.h' | while read header; do
-    HEADER_NAME=`echo $header | sed 's|\.h$||'`
-    HEADER_BASENAME=`basename $header`
-    echo "Generating $HEADER_NAME..."
-    echo "#include \"./$HEADER_BASENAME\"" > $HEADER_NAME
-done
