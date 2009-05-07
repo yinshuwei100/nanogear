2d45d4c5a1803e65f22f9c9b1f43e0885723a37a
Lorenzo Villani <lvillani@binaryhelix.net>
2009-05-07 15:28:23 +0200

    Add netbeans project directory
    committer: Lorenzo Villani <lvillani@binaryhelix.net>

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,16 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Default configuration
+CND_PLATFORM_Default=GNU-Linux-x86
+CND_ARTIFACT_DIR_Default=Debug/examples/forms
+CND_ARTIFACT_NAME_Default=forms
+CND_ARTIFACT_PATH_Default=Debug/examples/forms/forms
+CND_PACKAGE_DIR_Default=dist/Default/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Default=nanogear.tar
+CND_PACKAGE_PATH_Default=dist/Default/GNU-Linux-x86/package/nanogear.tar
